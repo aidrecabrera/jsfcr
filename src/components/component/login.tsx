0281c436cb47b888fa6af42a478013dc6e172d87
Svene <hello.svene@gmail.com>
2024-06-10 00:24:17 +0800

    tanstack router

---
+++ src/components/component/login.tsx
@@ -0,0 +1,69 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "@tanstack/react-router";
+
+export function login() {
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-balance text-muted-foreground">
+              Enter your email below to login to your account
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                placeholder="m@example.com"
+                required
+                type="email"
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <Link
+                  className="inline-block ml-auto text-sm underline"
+                  href="#"
+                >
+                  Forgot your password?
+                </Link>
+              </div>
+              <Input id="password" required type="password" />
+            </div>
+            <Button className="w-full" type="submit">
+              Login
+            </Button>
+            <Button className="w-full" variant="outline">
+              Login with Google
+            </Button>
+          </div>
+          <div className="mt-4 text-sm text-center">
+            Don't have an account?
+            <Link className="underline" href="#">
+              Sign up
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <img
+          alt="Image"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          height="1080"
+          src="/placeholder.svg"
+          style={{
+            aspectRatio: "1920/1080",
+            objectFit: "cover",
+          }}
+          width="1920"
+        />
+      </div>
+    </div>
+  );
+}
