a66a776946d9acad36131a4a784455ecf99c30b4
JSFCR <info.jsfcr@gmail.com>
2024-06-10 00:43:26 +0800

    login page development progress

--- src/components/component/login.tsx
+++ src/components/component/login.tsx
@@ -1,11 +1,28 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Link } from "@tanstack/react-router";
 
 export function Login() {
   return (
     <div className="w-full h-screen lg:grid lg:grid-cols-2">
+      <div className="hidden bg-muted lg:block">
+        <div className="relative w-full h-full">
+          <img
+            alt="Image"
+            className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            height="1080"
+            src="https://online.jhcsc.edu.ph/img/red.jpg"
+            width="1920"
+          />
+          <div className="absolute inset-0 flex items-center justify-center bg-green-700 bg-opacity-65">
+            <div className="absolute top-0 left-0 flex flex-row items-center justify-start gap-3 m-8">
+              <h1 className="text-2xl font-medium text-white text-ellipsis font-tight">
+                JH Cerilles State College
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
@@ -27,42 +44,14 @@ export function Login() {
             <div className="grid gap-2">
               <div className="flex items-center">
                 <Label htmlFor="password">Password</Label>
-                <Link
-                  className="inline-block ml-auto text-sm underline"
-                  href="#"
-                >
-                  Forgot your password?
-                </Link>
               </div>
               <Input id="password" required type="password" />
             </div>
             <Button className="w-full" type="submit">
               Login
             </Button>
-            <Button className="w-full" variant="outline">
-              Login with Google
-            </Button>
-          </div>
-          <div className="mt-4 text-sm text-center">
-            Don't have an account?
-            <Link className="underline" href="#">
-              Sign up
-            </Link>
           </div>
         </div>
-      </div>
-      <div className="hidden bg-muted lg:block">
-        <img
-          alt="Image"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-          height="1080"
-          src="/placeholder.svg"
-          style={{
-            aspectRatio: "1920/1080",
-            objectFit: "cover",
-          }}
-          width="1920"
-        />
       </div>
     </div>
   );
